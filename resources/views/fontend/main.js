ebc5388e37fc26716fe3a0b64ca60ead5cd4b196
kali <verrandyb@gmail.com>
2021-06-14 19:45:42 +0700

    bismillah

--- resources/views/fontend/main.js
+++ resources/views/fontend/main.js
@@ -15,7 +15,7 @@ Vue.prototype.$moment = moment
 console.log(process.env.MIX_BASE_URL)
 console.log(process.MIX_BASE_URL)
 
- axios.defaults.baseURL = "http://www.nabungyuk.id/"
+axios.defaults.baseURL = "http://www.nabungyuk.id/"
 // if(process.env.MIX_ENV_MODE == "production") {
 //     axios.defaults.baseURL = process.env.MIX_BASE_URL
 // }
