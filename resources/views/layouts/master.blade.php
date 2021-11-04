87c21e07e12abefc795b8a63fdfd2a600e01cadb
YourMasterIsFool <verrandyb@gmail.com>
2021-11-04 20:28:47 +0700

    first deploy

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <link href="/css/app.css" rel="stylesheet">
+   <link href="css/app.css" rel="stylesheet">
    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
    <!-- <link rel="shortcut icon" href="{{ asset('images/logoM.png')}}" href=""> -->
    <link rel="preconnect" href="//fonts.gstatic.com">
@@ -19,7 +19,7 @@
     <div id="app">
         <App/>
     </div>
-    <script src="/js/app.js"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
 
